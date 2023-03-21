afbd5e9589599f68bda793e6092fabd7b99832c2
Cindy Ho <cho102@ucr.edu>
2023-03-21 20:09:14 +0000

    added run.sh

--- ProjectD7/run.sh
+++ ProjectD7/run.sh
@@ -1,4 +1,4 @@
 mvn clean package
 spark-submit --class edu.ucr.cs.cs167.cho102.App --master "local[*]" target/ProjectD7-1.0-SNAPSHOT.jar Tweets_10k.json.bz2
-spark-submit --class edu.ucr.cs.cs167.cho102.Task2 --master "local[*]" target/ProjectD7-1.0-SNAPSHOT.jar
-spark-submit --class edu.ucr.cs.cs167.cho102.Task3 --master "local[*]" target/ProjectD7-1.0-SNAPSHOT.jar
+spark-submit --class edu.ucr.cs.cs167.cho102.Task2 --master "local[*]" target/ProjectD7-1.0-SNAPSHOT.jar tweets_clean.json tweets_topic.json
+spark-submit --class edu.ucr.cs.cs167.cho102.Task3 --master "local[*]" target/ProjectD7-1.0-SNAPSHOT.jar tweets_topic.json
